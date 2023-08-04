9e3dbcfd48568908b8cdc3b869b4465c28fc7a97
Huzaifa <zaifali785@gmail.com>
2023-08-04 17:35:11 +0500

    count movies problem

--- app/api/Random/route.ts
+++ app/api/Random/route.ts
@@ -2,10 +2,14 @@ import Movies from "@/model/Movies";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const movieCount = await Movies.count()
-    const randomIndex = Math.floor(Math.random() * movieCount)
-    const randomMovie = await Movies.find().skip(randomIndex).limit(1)
+    const movieCount = await Movies.aggregate([{ $group: { _id: null, count: { $sum: 1 } } }])
+    const totalMovies = movieCount[0].count;
+    const randomIndex = Math.floor(Math.random() * totalMovies);
+
+    const randomMovie = await Movies.aggregate([
+        { $skip: randomIndex },
+        { $limit: 1 }
+    ]);
 
     return NextResponse.json({ success: true, movie: randomMovie[0] }, { status: 200 })
-
 }
