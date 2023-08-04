786afa9c74c5d1614770fb85fa946f82f528ba96
Huzaifa <zaifali785@gmail.com>
2023-08-04 18:01:02 +0500

    problem solution

--- app/api/Random/route.ts
+++ app/api/Random/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const movieCount = await Movies.aggregate([{ $group: { _id: null, count: { $sum: 1 } } }])
-    const totalMovies = movieCount[0].count;
-    const randomIndex = Math.floor(Math.random() * totalMovies);
+    const totalMovies = await movieCount[0].count;
+    const randomIndex =  Math.floor(Math.random() * totalMovies);
 
     const randomMovie = await Movies.aggregate([
         { $skip: randomIndex },
